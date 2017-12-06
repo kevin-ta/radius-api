42e2962f29733da532288def03b631dcd8d99abf
Kévin Ta <kevin-ta@users.noreply.github.com>
2017-12-06 14:05:18 +0800

    Changed error code to 403

--- app/auth/VerifyRole.js
+++ app/auth/VerifyRole.js
@@ -6,7 +6,7 @@ function verifyRole(req, res, next) {
     if (!token) return res.status(403).send({ auth: false, message: 'No token provided.' });
     jwt.verify(token, config.secret, function(err, decoded) {   
         if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-        if (decoded.role != 'admin') return res.status(500).send({ auth: false, message: 'Unauthorized user.' });
+        if (decoded.role != 'admin') return res.status(403).send({ auth: false, message: 'Unauthorized user.' });
         req.userId = decoded.id;
         next();
     });
